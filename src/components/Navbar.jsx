70baacf3af98a2656bf4b5e8fae3886243aa70a6
Saim Hayyat <saimhayatrwp2007@gmail.com>
2025-06-03 21:45:57 +0500

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,49 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Navbar () {
-  const[showMenu , setShowMenu] = useState(false)
-  const handleToggle = ()=>{
-    setShowMenu(!showMenu)
-  }
-    return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" onClick={handleToggle} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      {
-       showMenu ? <i class="bi bi-x-lg"></i> : <i class= "navbar-toggler-icon"></i>
-      }
-      
-    </button>
-    <div class={`collapse navbar-collapse ${showMenu ? 'show' :''}`} id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link"  href="#"><i class="bi bi-person-fill"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><i class="bi bi-cart4"></i></a>
-        </li>
-        
-        <li class="nav-item">
-          <a class="nav-link Services" aria-disabled="false"><i class="bi bi-search"></i></a>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
- )
+export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleToggle = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button
+          className="navbar-toggler"
+          onClick={handleToggle}
+          type="button"
+          aria-controls="navbarSupportedContent"
+          aria-expanded={showMenu}
+          aria-label="Toggle navigation"
+        >
+          {showMenu ? (
+            <i className="bi bi-x-lg"></i>
+          ) : (
+            <span className="navbar-toggler-icon"></span>
+          )}
+        </button>
+        <div
+          className={`collapse navbar-collapse ${showMenu ? 'show' : ''}`}
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#"><i className="bi bi-person-fill"></i></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#"><i className="bi bi-cart4"></i></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link Services" href="#"><i className="bi bi-search"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
+
 
 
