20e897fafe9d69ddaed2550d14cb5ce6d0a7f591
Saim Hayyat <saimhayatrwp2007@gmail.com>
2025-06-04 14:46:27 +0500

    fixed classs issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ export default function Navbar() {
   };
 
   return (
-    <nav className=Name="navbar navbar-expand-lg bg-body-tertiary">
-      <div className=Name="container">
-        <a className=Name="navbar-brand" href="#">Navbar</a>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <a className="navbar-brand" href="#">Navbar</a>
         <button
-          className=Name="navbar-toggler"
+          className="navbar-toggler"
           onClick={handleToggle}
           type="button"
           aria-controls="navbarSupportedContent"
@@ -19,24 +19,24 @@ export default function Navbar() {
           aria-label="Toggle navigation"
         >
           {showMenu ? (
-            <i className=Name="bi bi-x-lg"></i>
+            <i className="bi bi-x-lg"></i>
           ) : (
-            <span className=Name="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           )}
         </button>
         <div
-          className=Name={`collapse navbar-collapse ${showMenu ? 'show' : ''}`}
+          className={`collapse navbar-collapse ${showMenu ? 'show' : ''}`}
           id="navbarSupportedContent"
         >
-          <ul className=Name="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className=Name="nav-item">
-              <a className=Name="nav-link" href="#"><i className=Name="bi bi-person-fill"></i></a>
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#"><i className="bi bi-person-fill"></i></a>
             </li>
-            <li className=Name="nav-item">
-              <a className=Name="nav-link" href="#"><i className=Name="bi bi-cart4"></i></a>
+            <li className="nav-item">
+              <a className="nav-link" href="#"><i className="bi bi-cart4"></i></a>
             </li>
-            <li className=Name="nav-item">
-              <a className=Name="nav-link Services" href="#"><i className=Name="bi bi-search"></i></a>
+            <li className="nav-item">
+              <a className="nav-link Services" href="#"><i className="bi bi-search"></i></a>
             </li>
           </ul>
         </div>
