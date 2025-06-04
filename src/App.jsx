2e656a79b3e3fdf07cf7cbee201840fe154bc365
Saim Hayyat <saimhayatrwp2007@gmail.com>
2025-06-04 20:37:38 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,72 @@
 
 import Navbar from './components/Navbar';
 import './App.css'
+import ProductCard from './components/ProductCard';
 
 export default function App ()
 {
   return ( <>
   
   <Navbar/>
+
+  <div className="container">
+    <div className="row">
+      <div className="col-md-4">
+<ProductCard/>
+      </div>
+      <div className="col-md-4">
+<ProductCard/>
+      </div>
+      <div className="col-md-4">
+<ProductCard/>
+      </div>
+      <div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div><div className="col-md-4">
+<ProductCard/>
+      </div>
+    </div>
+
+  </div>
+  
    </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,73 @@
-import { useState } from "react";
+
 
 export default function Navbar() {
-  const [showMenu, setShowMenu] = useState(false);
-  const handleToggle = () => {
-    setShowMenu(!showMenu);
-  };
-
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button
-          className="navbar-toggler"
-          onClick={handleToggle}
-          type="button"
-          aria-controls="navbarSupportedContent"
-          aria-expanded={showMenu}
-          aria-label="Toggle navigation"
-        >
-          {showMenu ? (
-            <i className="bi bi-x-lg"></i>
-          ) : (
-            <span className="navbar-toggler-icon"></span>
-          )}
-        </button>
-        <div
-          className={`collapse navbar-collapse ${showMenu ? 'show' : ''}`}
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="#"><i className="bi bi-person-fill"></i></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#"><i className="bi bi-cart4"></i></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link Services" href="#"><i className="bi bi-search"></i></a>
-            </li>
-          </ul>
+
+
+ <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      
+  <img className="nav-logo" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4HDjX7UNfx023R6yPhZOGHcqDt392vBusvQ&s" alt="" />
+
+
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Documentation
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item is-selected">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider"/>
+          <a class="navbar-item">
+            Report an issue
+          </a>
         </div>
       </div>
-    </nav>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
   );
 }
 
